3262ffac8d658ea6e955726544290ece50c96c30
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-10 00:20:06 +0100

    Improved styling for souls

--- src/components/common/calculator.svelte
+++ src/components/common/calculator.svelte
@@ -4,10 +4,8 @@
 
   export let soulDefinitions: SoulDefinition[];
 
-  let soulCalculations: SoulCalculation[] = soulDefinitions.map((definition) => ({
-    definition: definition,
-    quantity: 0,
-  }));
+  let soulCalculations: SoulCalculation[];
+  reset();
 
   $: soulsTotal = soulCalculations.reduce<number>((prev, soul) => {
     return prev + soul.definition.value * soul.quantity;
@@ -27,28 +25,39 @@
     soulCalculations[index].quantity = Math.max(0, newQuantity);
     soulCalculations = soulCalculations;
   }
+
+  function reset() {
+    soulCalculations = soulDefinitions.map((definition) => ({
+      definition: definition,
+      quantity: 0,
+    }));
+  }
 </script>
 
-<div class="flex flex-row flex-wrap">
-  {#each soulCalculations as soul, i}
-    <div class="grow border border-solid rounded-lg p-3 m-1">
-      <div class="mb-1 text-center text-base">{soul.definition.name}</div>
-      <div class="mb-1 text-center text-xs">{soul.definition.value.toLocaleString()}</div>
-      <div class="flex items-center justify-between">
-        <button
-          on:click={() => decrement(i)}
-          class="bg-orange-dark rounded-lg p-4 shadow-md text-base">-</button
-        >
-        <div class="text-base">{soul.quantity}</div>
-        <button
-          on:click={() => increment(i)}
-          class="bg-orange-dark rounded-lg p-4 shadow-md text-base">+</button
-        >
+<div class="flex flex-col">
+  <div class="grid grid-cols-2 gap-1">
+    {#each soulCalculations as soul, i}
+      <div class="border border-solid rounded-lg p-3">
+        <div class="mb-1 text-center text-base">{soul.definition.name}</div>
+        <div class="mb-1 text-center text-xs">{soul.definition.value.toLocaleString()}</div>
+        <div class="flex items-center justify-between">
+          <button
+            on:click={() => decrement(i)}
+            class="bg-orange-dark rounded-lg p-4 shadow-md text-base w-20">-</button
+          >
+          <div class="text-base w-20 text-center">{soul.quantity.toLocaleString()}</div>
+          <button
+            on:click={() => increment(i)}
+            class="bg-orange-dark rounded-lg p-4 shadow-md text-base w-20">+</button
+          >
+        </div>
       </div>
-    </div>
-  {/each}
-</div>
+    {/each}
+  </div>
 
-<div class="text-xl">
-  Total: {soulsTotal.toLocaleString()}
+  <div class="absolute bottom-0 index-x mt-1 h-20 border">
+    <div class="text-xl">
+      Total: {soulsTotal.toLocaleString()}
+    </div>
+  </div>
 </div>

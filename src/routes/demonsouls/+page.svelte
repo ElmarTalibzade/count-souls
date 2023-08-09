3cbacb5557cacf73d3c9440f020735b47b15f22f
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-09 23:46:11 +0100

    Added all soul definitions for Demon Souls

--- src/routes/demonsouls/+page.svelte
+++ src/routes/demonsouls/+page.svelte
@@ -6,6 +6,11 @@
     { name: 'Unknown Warrior Soul', value: 200 },
     { name: 'Unknown Hero Soul', value: 400 },
     { name: 'Renowned Warrior Soul', value: 800 },
+    { name: 'Renowned Hero Soul', value: 1000 },
+    { name: 'Storied Warrior Soul', value: 2000 },
+    { name: 'Storied Hero Soul', value: 4000 },
+    { name: 'Legendary Warrior Soul', value: 8000 },
+    { name: 'Legendary Hero Soul', value: 10000 },
   ];
 </script>
 

ce3096ce3bd7ac2e301085a1ab2b5b40eeb09f8f
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-10 00:46:32 +0100

    Added basic menu item

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<ul>
+  <li><a href="/des">Demon Souls + Remake</a></li>
+  <li><a href="/ds1">Dark Souls I + Remastered</a></li>
+  <li>Dark Souls II</li>
+  <li>Dark Souls III</li>
+  <li>Bloodborne</li>
+  <li>Elden Ring</li>
+</ul>

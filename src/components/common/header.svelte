796c329478aa039668e95e9d1b069e5522264189
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-10 21:13:50 +0100

    Navbar is now fixed

--- src/components/common/header.svelte
+++ src/components/common/header.svelte
@@ -2,7 +2,7 @@
   import logo from '../../assets/logo.svg';
 </script>
 
-<div class="flex items-stretch">
+<div class="flex sticky left-0 top-0 items-stretch bg-white">
   <img class="flex-none w-8 p-1" src={logo} />
   <div class="flex-auto">
     <h1 class="text-3xl font-bold">Count Souls</h1>

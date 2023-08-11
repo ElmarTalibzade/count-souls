7429b6a7e1ced2bebe2d51056a01ed5c526e6913
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-11 22:10:21 +0100

    Updated homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
-<div class="space-y-2 text-center">
+<script lang="ts">
+  import menu from '../assets/icon_menu.svg';
+</script>
+
+<div class="space-y-6 p-5 text-center text-lg">
   <p>Hello, and welcome to Count Souls - a one stop destination for your soul counting needs!</p>
   <p>
     If you ever found yourself whipping out a calculator to determine how many souls you need to
-    use, then this is a right place for you.
+    consume while referring to a wiki page, then you're at the right place.
   </p>
-  <p>Just click the menu above to get started!</p>
+  <p>
+    Just click the <span class="inline-flex items-baseline"
+      ><img class="w-3" src={menu} alt="Menu" /></span
+    > menu above to get started!
+  </p>
 </div>

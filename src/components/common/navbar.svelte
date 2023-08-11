0091103f2e14a35cd63d84c6364636990fce0d90
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-11 18:40:36 +0100

    Increased navbar height and added border

--- src/components/common/navbar.svelte
+++ src/components/common/navbar.svelte
@@ -2,7 +2,7 @@
   import logo from '../../assets/logo.svg';
 </script>
 
-<div class="flex sticky left-0 top-0 items-stretch bg-white">
+<div class="flex sticky left-0 top-0 bg-white border-b h-14 flex-row items-center">
   <img class="flex-none w-8 p-1" src={logo} />
   <div class="flex-auto">
     <h1 class="text-3xl font-bold">Count Souls</h1>

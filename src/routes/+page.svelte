571b66be5dacac97e3425462e7c70fccf7e66078
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-11 18:40:53 +0100

    Added intro paragraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
+<div class="text-center space-y-2">
+  <p>Hello, and welcome to Count Souls - a one stop destination for your soul counting needs!</p>
+  <p>
+    If you ever found yourself whipping out a calculator to determine how many souls you need to
+    use, then this is a right place for you.
+  </p>
+  <p>Just select a game to get counting!</p>
+</div>
+
 <ul>
   <li><a href="/des">Demon Souls + Remake</a></li>
   <li><a href="/ds1">Dark Souls I + Remastered</a></li>

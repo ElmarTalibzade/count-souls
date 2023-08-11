39c4a3f6d00894882ffa194d97c73281f0eb5c11
Elmar Talibzade <elmar.talibzade@gmail.com>
2023-08-11 21:38:32 +0100

    Added calculations for Dark Souls 2

---
+++ src/routes/ds2/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Calculator from '../../components/common/calculator.svelte';
+  import type Soul from 'models/soulDefinition';
+
+  const souls: Soul[] = [
+    { name: 'Fading Soul', value: 50 },
+    { name: 'Soul of a Lost Undead', value: 200 },
+    { name: 'Large Soul of a Lost Undead', value: 500 },
+    { name: 'Soul of a Nameless Soldier', value: 800 },
+    { name: 'Large Soul of a Nameless Soldier', value: 1000 },
+    { name: 'Soul of a Proud Knight', value: 2000 },
+    { name: 'Large Soul of a Proud Knight', value: 3000 },
+    { name: 'Soul of a Brave Warrior', value: 5000 },
+    { name: 'Large Soul of a Brave Warrior', value: 8000 },
+    { name: 'Soul of a Hero', value: 10000 },
+    { name: 'Soul of a Great Hero', value: 20000 },
+  ];
+</script>
+
+<svelte:head>
+  <title>Dark Souls 2</title>
+</svelte:head>
+
+<Calculator soulDefinitions={souls} />
